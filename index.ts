8ec89e83cd845e027c5b2e20eb4c93efe96c3d9c
Filippo Scelfo <scelfofilippo@gmail.com>
2023-02-03 18:28:10 +0100

    Add Passport with Github Authentication Strategy

--- index.ts
+++ index.ts
@@ -1,9 +1,16 @@
 import express from "express";
-import planetsRoutes from "./src/routes/planets";
 
 import { initCorsMiddleware } from "./src/lib/middleware/cors";
+import { initSessionMiddleware } from "./src/lib/middleware/session";
+import { passport } from "./src/lib/middleware/passport";
+
+import planetsRoutes from "./src/routes/planets";
 
 const app = express();
+
+app.use(initSessionMiddleware());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.json());
 
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,22 @@
 import express from "express";
-import planetsRoutes from "./routes/planets";
 
 import { initCorsMiddleware } from "./lib/middleware/cors";
+import { initSessionMiddleware } from "./lib/middleware/session";
+import { passport } from "./lib/middleware/passport";
+
+import planetsRoutes from "./routes/planets";
 
 const app = express();
 
+app.use(initSessionMiddleware());
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.json());
 
-app.use(initCorsMiddleware);
-
 app.use("/planets", planetsRoutes);
+
+app.use(initCorsMiddleware);
 
 app.listen(3000, () => {
   console.log("Running on port", 3000);
